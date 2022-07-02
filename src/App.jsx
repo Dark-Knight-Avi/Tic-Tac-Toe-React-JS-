5cfe17ff16091525cab7e78cac302bf4e6aaacea
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-07-02 10:54:33 +0530

    winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./components/Board";
+import { calculateWinner } from "./helpers";
 import "./style/root.scss"
-export default () => (
-  <div className="app h-[100vh] flex items-center justify-center">
-    <h1 className="mb-20 text-5xl">TIC TAC TOE</h1>
-    <Board/>
-  </div>
-);
+const App = () => {
+
+  const [board , setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+  const winner = calculateWinner(board); 
+  const message = winner ? `Winner is ${winner}` : `Next player is ${isXNext ? 'X' : 'O'}`;
+  const handleSquareClick = (position) => {
+    if(board[position] || winner){
+      return;
+    }
+    setBoard( (prev) => {
+        return prev.map((square, pos) => {
+          if(pos === position){
+            return isXNext ? 'X' : 'O'
+          }
+          else {
+            return square;
+          }
+        })
+    } )
+    setIsXNext((prev) => !prev)
+  }
+  return(
+    <div className="app h-[100vh] flex items-center justify-center">
+      <h1 className="mb-2 text-5xl">TIC TAC TOE</h1>
+      <h2 className="mb-20">{message}</h2>
+      <Board board = { board } handleSquareClick = { handleSquareClick }/>
+    </div>
+  );
+};
+
+export default App;
